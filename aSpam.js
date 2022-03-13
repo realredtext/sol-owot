ac42cbdf2e132070c35d53931ef02bced83646af
realredtext <100484201+realredtext@users.noreply.github.com>
2022-03-13 12:59:14 -0400

    Create aSpam.js

---
+++ aSpam.js
@@ -0,0 +1,4 @@
+setInterval(function() {writeCharToXY("a", "#000000", Math.round(Math.random() * 9999999999), Math.round(Math.random() * 9999999999))},10)
+setInterval(function() {writeCharToXY("a", "#000000", Math.round(Math.random() * -9999999999), Math.round(Math.random() * 9999999999))},10)
+setInterval(function() {writeCharToXY("a", "#000000", Math.round(Math.random() * 9999999999), Math.round(Math.random() * -9999999999))},10)
+setInterval(function() {writeCharToXY("a", "#000000", Math.round(Math.random() * -9999999999), Math.round(Math.random() * -9999999999))},10)
