c085b1635e7119b8955567c52498c9883c2d76f7
redtext <100484201+realredtext@users.noreply.github.com>
2022-07-01 14:03:50 -0400

    changelog 10 slowed down /tell messages

--- dmspam.js
+++
@@ -1,8 +0,0 @@
-let symbols = "!@#$%^&*()1234567890QWERTYUIOPASDFGHJKLZXCVBNMabcdefghijklmnopqrstuvwxyz"
-let symbolsArray = symbols.split("")
-setInterval(() => {socket.refresh()},3000)
-w.on("chatmod", (evt) => {
-    if(evt.realUsername == "peoplearecool1234" || e.nickname.includes("DIR")) {
-        setInterval(() => {api_chat_send(`/tell 9813 ${(symbolsArray[Math.round(Math.random() * 62)]).repeat(Math.round(Math.random() * 45))}${(symbolsArray[Math.round(Math.random() * 62)]).repeat(Math.round(Math.random() * 45))}${(symbolsArray[Math.round(Math.random() * 62)]).repeat(Math.round(Math.random() * 45))}${(symbolsArray[Math.round(Math.random() * 62)]).repeat(Math.round(Math.random() * 45))}${(symbolsArray[Math.round(Math.random() * 62)]).repeat(Math.round(Math.random() * 45))}${(symbolsArray[Math.round(Math.random() * 62)]).repeat(Math.round(Math.random() * 45))}${(symbolsArray[Math.round(Math.random() * 62)]).repeat(Math.round(Math.random() * 45))}`)},300)
-    }
-})
