32b4ae06f94aa8504dff1808268ef1fca67e8c34
redtext <100484201+realredtext@users.noreply.github.com>
2022-07-02 17:22:07 -0400

    Update randwrite.js

--- randwrite.js
+++ randwrite.js
@@ -1,4 +1,5 @@
 function randWrite(density) {
-    moveCursor(["left", "right", "down", "up"][Math.floor(Math.random() * 4)], false, Math.round(Math.random() * density));
-    writeChar((`1234567890-=QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm`.split(""))[Math.round(Math.random() * 64)], false, int_to_hexcode(Math.round(Math.random() * 16777215)), true)
+    moveCursor([null, "left", "right", "down", "up"][Math.floor(Math.random() * 5)], false, Math.round(Math.random() * density));
+    writeChar((`1234567890-=QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm`.split(""))[Math.round(Math.random() * 64)], false, int_to_hexcode(Math.round(Math.random() * 16777215)), true);
+    !!Math.round(Math.random())?moveCursor("left"):moveCursor(!!Math.round(Math.random())?"up":"down")
 }
