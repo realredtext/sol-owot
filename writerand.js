796702a300229585af55b3252926e7e40c1cb581
redtext <100484201+realredtext@users.noreply.github.com>
2022-07-17 16:46:11 -0400

    Update writerand.js

--- writerand.js
+++ writerand.js
@@ -1,4 +1,4 @@
 function writeRand(dist) {
     let symbs = "`1234567890-=+~!@#$%^&*()_+QWERTYUIOP{}ASDFGHJKL:\"ZXCVBNM<>?|".split("");
-        writeCharToXY(symbs[Math.round(Math.random() * symbs.length)], int_to_hexcode(Math.round(Math.random() * 16777216)), !!Math.round(Math.random())?Math.round(Math.random() * dist): -Math.round(Math.random() * dist), !!Math.round(Math.random())?Math.round(Math.random() * dist): -Math.round(Math.random() * dist))
+        writeCharToXY(symbs[Math.round(Math.random() * symbs.length)], Math.round(Math.random() * 16777215), !!Math.round(Math.random())?Math.round(Math.random() * dist): -Math.round(Math.random() * dist), !!Math.round(Math.random())?Math.round(Math.random() * dist): -Math.round(Math.random() * dist))
 }
