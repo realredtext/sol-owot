83d278c65596eb8b94ac451ecf3e1b24420bdb3e
redtext <100484201+realredtext@users.noreply.github.com>
2022-07-27 20:22:01 -0400

    Update crt.js

--- crt.js
+++ crt.js
@@ -2,7 +2,7 @@ function rangedRandom(min, max) {
     return min + Math.round(Math.random() *(max - min));
 };
 
-function colorRandomTile(minX, maxX, minY, maxY) {
+function colorSplatter(minX, maxX, minY, maxY) {
     for(let e = 0; e < 8; e++) {
         for(let i = 0; i < 16; i++) {
             writeBuffer.push([rangedRandom(minY, maxY), rangedRandom(minX, maxX), x, e, i, Date.now, "█", nextObjId, color]);
@@ -10,4 +10,4 @@ function colorRandomTile(minX, maxX, minY, maxY) {
     };
 };
 
-// setInterval(()=>{colorRandomTile(-20, 20, -20, 20)},1000)
+// setInterval(()=>{colorSplatter(-20, 20, -20, 20)},1000)
