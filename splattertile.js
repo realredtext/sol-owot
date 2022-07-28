f8526fe2c0e2ebac4f9f6cd35945381f4b2e3254
redtext <100484201+realredtext@users.noreply.github.com>
2022-07-27 20:29:33 -0400

    Create splattertile.js

---
+++ splattertile.js
@@ -0,0 +1,7 @@
+function splatterTile(x, y) {
+    for(let e = 0; e < 8; e++) {
+        for(let i = 0; i < 16; i++) {
+            writeBuffer.push([y, x, e, i, Date.now(), "█", nextObjId, Math.round(Math.random() * 16777215)])
+        }
+    }
+}
