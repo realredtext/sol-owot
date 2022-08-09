7882ae8d38bfa59aada4e0108f6448586f21a8e3
redtext <100484201+realredtext@users.noreply.github.com>
2022-08-09 16:58:14 -0400

    Update splattertile.js

--- splattertile.js
+++ splattertile.js
@@ -1,7 +1,7 @@
 function splatterTile(x, y) {
     for(let e = 0; e < 8; e++) {
         for(let i = 0; i < 16; i++) {
-            writeBuffer.push([y, x, e, i, Date.now(), "█", nextObjId, Math.round(Math.random() * 16777215)])
-        }
-    }
-}
+            writeBuffer.push([y, x, e, i, Date.now(), "█", nextObjId, Math.round(Math.random() * 16777215)]);
+        };
+    };
+};
