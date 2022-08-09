87bd7250dbda8d0cc09f3bbb5baa557b1a8a29e1
redtext <100484201+realredtext@users.noreply.github.com>
2022-08-09 16:49:34 -0400

    Update spamfunction.js

--- spamfunction.js
+++ spamfunction.js
@@ -4,6 +4,6 @@ function spam(str, len, wid) {
     s = s.repeat(wid);
     for(var i = 0; i < s.length; i++) {
         var c = s[i];
-        writeChar(c,false)
-    }
-}
+        writeChar(c,false);
+    };
+};
