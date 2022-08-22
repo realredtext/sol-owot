c1405286dcaa44576ab70e76ad06a978b2d7e829
redtext <100484201+realredtext@users.noreply.github.com>
2022-08-22 17:27:39 -0400

    Update crt.js

--- crt.js
+++ crt.js
@@ -3,10 +3,20 @@ function rangedRandom(min, max) {
 };
 
 function colorSplatter(minX, maxX, minY, maxY) { //tile dimensions
-    for(let e = 0; e < 8; e++) {
-        for(let i = 0; i < 16; i++) {
-            writeBuffer.push([rangedRandom(minY, maxY), rangedRandom(minX, maxX), e, i, Date.now, "█", nextObjId, Math.round(Math.random() * 16777214)]);
-        };
+    for(var charX = 0; charX < 16; charX++) {
+        socket.send(JSON.stringify({
+            kind: "write",
+            edits: [
+                [rangedRandom(minY, maxY), rangedRandom(minX, maxX), 0, charX, Date.now(), "█", 10, Math.floor(Math.random() * 16777216)],
+                [rangedRandom(minY, maxY), rangedRandom(minX, maxX), 1, charX, Date.now(), "█", 10, Math.floor(Math.random() * 16777216)],
+                [rangedRandom(minY, maxY), rangedRandom(minX, maxX), 2, charX, Date.now(), "█", 10, Math.floor(Math.random() * 16777216)],
+                [rangedRandom(minY, maxY), rangedRandom(minX, maxX), 3, charX, Date.now(), "█", 10, Math.floor(Math.random() * 16777216)],
+                [rangedRandom(minY, maxY), rangedRandom(minX, maxX), 4, charX, Date.now(), "█", 10, Math.floor(Math.random() * 16777216)],
+                [rangedRandom(minY, maxY), rangedRandom(minX, maxX), 5, charX, Date.now(), "█", 10, Math.floor(Math.random() * 16777216)],
+                [rangedRandom(minY, maxY), rangedRandom(minX, maxX), 6, charX, Date.now(), "█", 10, Math.floor(Math.random() * 16777216)],
+                [rangedRandom(minY, maxY), rangedRandom(minX, maxX), 7, charX, Date.now(), "█", 10, Math.floor(Math.random() * 16777216)]
+            ]
+        }));
     };
 };
 
