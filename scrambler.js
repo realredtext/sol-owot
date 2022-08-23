a287e114522d383f9738bd87cef632d4847371b1
redtext <100484201+realredtext@users.noreply.github.com>
2022-08-23 18:27:28 -0400

    Create scrambler.js

---
+++ scrambler.js
@@ -0,0 +1,18 @@
+//do NOT use on main page, super shit idea
+for(const i in tiles) {
+    const [ty, tx] = i.split(",").map(num => Number(num));
+
+    for(var x = 0; x < 16; x++) {
+        socket.send(JSON.stringify({
+            kind: "write",
+            edits: [[ty, tx, 0, x, Date.now(), base64table[Math.floor(Math.random() * base64table.length)], 10, Math.floor(Math.random() * 16777216)],
+                   [ty, tx, 1, x, Date.now(), base64table[Math.floor(Math.random() * base64table.length)], 10, Math.floor(Math.random() * 16777216)],
+                   [ty, tx, 2, x, Date.now(), base64table[Math.floor(Math.random() * base64table.length)], 10, Math.floor(Math.random() * 16777216)],
+                   [ty, tx, 3, x, Date.now(), base64table[Math.floor(Math.random() * base64table.length)], 10, Math.floor(Math.random() * 16777216)],
+                   [ty, tx, 4, x, Date.now(), base64table[Math.floor(Math.random() * base64table.length)], 10, Math.floor(Math.random() * 16777216)],
+                   [ty, tx, 5, x, Date.now(), base64table[Math.floor(Math.random() * base64table.length)], 10, Math.floor(Math.random() * 16777216)],
+                   [ty, tx, 6, x, Date.now(), base64table[Math.floor(Math.random() * base64table.length)], 10, Math.floor(Math.random() * 16777216)],
+                   [ty, tx, 7, x, Date.now(), base64table[Math.floor(Math.random() * base64table.length)], 10, Math.floor(Math.random() * 16777216)]]
+        }));
+    };
+};
