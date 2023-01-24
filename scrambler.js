2e7a83306abc001475a61c29336757fbb2700e93
redtext <100484201+realredtext@users.noreply.github.com>
2023-01-23 20:33:19 -0500

    Update scrambler.js

--- scrambler.js
+++ scrambler.js
@@ -5,14 +5,14 @@ for(const i in tiles) {
     for(var x = 0; x < 16; x++) {
         socket.send(JSON.stringify({
             kind: "write",
-            edits: [[ty, tx, 0, x, Date.now(), base64table[Math.floor(Math.random() * base64table.length)], 10, Math.floor(Math.random() * 16777216)],
-                   [ty, tx, 1, x, Date.now(), base64table[Math.floor(Math.random() * base64table.length)], 10, Math.floor(Math.random() * 16777216)],
-                   [ty, tx, 2, x, Date.now(), base64table[Math.floor(Math.random() * base64table.length)], 10, Math.floor(Math.random() * 16777216)],
-                   [ty, tx, 3, x, Date.now(), base64table[Math.floor(Math.random() * base64table.length)], 10, Math.floor(Math.random() * 16777216)],
-                   [ty, tx, 4, x, Date.now(), base64table[Math.floor(Math.random() * base64table.length)], 10, Math.floor(Math.random() * 16777216)],
-                   [ty, tx, 5, x, Date.now(), base64table[Math.floor(Math.random() * base64table.length)], 10, Math.floor(Math.random() * 16777216)],
-                   [ty, tx, 6, x, Date.now(), base64table[Math.floor(Math.random() * base64table.length)], 10, Math.floor(Math.random() * 16777216)],
-                   [ty, tx, 7, x, Date.now(), base64table[Math.floor(Math.random() * base64table.length)], 10, Math.floor(Math.random() * 16777216)]]
+            edits: [[ty, tx, 0, x, Date.now(), String.fromCharCode(Math.floor(Math.random()*144000)), 10, Math.floor(Math.random() * 16777216)],
+                   [ty, tx, 1, x, Date.now(), String.fromCharCode(Math.floor(Math.random()*144000)), 10, Math.floor(Math.random() * 16777216)],
+                   [ty, tx, 2, x, Date.now(), String.fromCharCode(Math.floor(Math.random()*144000)), 10, Math.floor(Math.random() * 16777216)],
+                   [ty, tx, 3, x, Date.now(), String.fromCharCode(Math.floor(Math.random()*144000)), 10, Math.floor(Math.random() * 16777216)],
+                   [ty, tx, 4, x, Date.now(), String.fromCharCode(Math.floor(Math.random()*144000)), 10, Math.floor(Math.random() * 16777216)],
+                   [ty, tx, 5, x, Date.now(), String.fromCharCode(Math.floor(Math.random()*144000)), 10, Math.floor(Math.random() * 16777216)],
+                   [ty, tx, 6, x, Date.now(), String.fromCharCode(Math.floor(Math.random()*144000)), 10, Math.floor(Math.random() * 16777216)],
+                   [ty, tx, 7, x, Date.now(), String.fromCharCode(Math.floor(Math.random()*144000)), 10, Math.floor(Math.random() * 16777216)]]
         }));
     };
 };
