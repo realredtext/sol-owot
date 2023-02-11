aef8cddd189a4e6e468b8d00042ece4810a2424f
redtext <100484201+realredtext@users.noreply.github.com>
2023-02-10 20:17:06 -0500

    speed improvement

--- splattertile.js
+++ splattertile.js
@@ -1,17 +1,17 @@
 function splatterTile(tx, ty) {
+    let edits = [];
     for(let cx = 0; cx < 16; cx++) {
-        socket.send(JSON.stringify({
-            kind: "write",
-            edits: [
-                [ty, tx, 0, cx, getDate(), "█", nextObjId++, Math.floor(Math.random() * 16777215)],
-                [ty, tx, 1, cx, getDate(), "█", nextObjId++, Math.floor(Math.random() * 16777215)],
-                [ty, tx, 2, cx, getDate(), "█", nextObjId++, Math.floor(Math.random() * 16777215)],
-                [ty, tx, 3, cx, getDate(), "█", nextObjId++, Math.floor(Math.random() * 16777215)],
-                [ty, tx, 4, cx, getDate(), "█", nextObjId++, Math.floor(Math.random() * 16777215)],
-                [ty, tx, 5, cx, getDate(), "█", nextObjId++, Math.floor(Math.random() * 16777215)],
-                [ty, tx, 6, cx, getDate(), "█", nextObjId++, Math.floor(Math.random() * 16777215)],
-                [ty, tx, 7, cx, getDate(), "█", nextObjId++, Math.floor(Math.random() * 16777215)]
-            ]
-        }));
+        edits.push([ty, tx, 0, cx, getDate, "█", 1, Math.floor(Math.random()*16777215)]);
+        edits.push([ty, tx, 1, cx, getDate, "█", 1, Math.floor(Math.random()*16777215)]);
+        edits.push([ty, tx, 2, cx, getDate, "█", 1, Math.floor(Math.random()*16777215)]);
+        edits.push([ty, tx, 3, cx, getDate, "█", 1, Math.floor(Math.random()*16777215)]);
+        edits.push([ty, tx, 4, cx, getDate, "█", 1, Math.floor(Math.random()*16777215)]);
+        edits.push([ty, tx, 5, cx, getDate, "█", 1, Math.floor(Math.random()*16777215)]);
+        edits.push([ty, tx, 6, cx, getDate, "█", 1, Math.floor(Math.random()*16777215)]);
+        edits.push([ty, tx, 7, cx, getDate, "█", 1, Math.floor(Math.random()*16777215)]);
     };
+    socket.send(JSON.stringify({
+        kind: "write",
+        edits: edits
+    }));
 };
