c268448f58cefcf8b3c2c59d183781ef1c5ee374
redtext <100484201+realredtext@users.noreply.github.com>
2023-02-10 20:18:12 -0500

    Update range_clearer.js

--- range_clearer.js
+++ range_clearer.js
@@ -1,43 +1,62 @@
 function clearTileRange(minX, minY, maxX, maxY) {
     let IntRange = (min, max) => {
         if(min > max) throw new RangeError("Minimun is greater than maximum!");
-    
+
         let list = [];
-    
+
         for(let i = min; i <= max; i++) {
             list.push(i);
         };
-    
+
         return list;
     };
 
-    let wipeTile = (tx, ty, char=" ") => {
+    window.wipeTile = function(tx, ty, char=" ") {
+        let edits = [];
         for(let x = 0; x < 16; x++) {
-            socket.send(JSON.stringify({
-                kind: "write",
-                edits: [
-                    [ty, tx, 0, x, getDate(), char, nextObjId++, 0],
-                    [ty, tx, 1, x, getDate(), char, nextObjId++, 0],
-                    [ty, tx, 2, x, getDate(), char, nextObjId++, 0],
-                    [ty, tx, 3, x, getDate(), char, nextObjId++, 0],
-                    [ty, tx, 4, x, getDate(), char, nextObjId++, 0],
-                    [ty, tx, 5, x, getDate(), char, nextObjId++, 0],
-                    [ty, tx, 6, x, getDate(), char, nextObjId++, 0],
-                    [ty, tx, 7, x, getDate(), char, nextObjId++, 0]
-                ]
-            }));
+            edits.push(
+                [ty, tx, 0, x, Date.now(), char, 0, 0],
+                [ty, tx, 1, x, Date.now(), char, 0, 0],
+                [ty, tx, 2, x, Date.now(), char, 0, 0],
+                [ty, tx, 3, x, Date.now(), char, 0, 0],
+                [ty, tx, 4, x, Date.now(), char, 0, 0],
+                [ty, tx, 5, x, Date.now(), char, 0, 0],
+                [ty, tx, 6, x, Date.now(), char, 0, 0],
+                [ty, tx, 7, x, Date.now(), char, 0, 0]
+            )
         };
+        socket.send(JSON.stringify({
+            kind: "write",
+            edits: edits
+        }));
+    };
+    if(state.userModel.is_member) window.wipeTile = function(tx, ty, char=" ") {
+        char = char[0];
+        socket.send(JSON.stringify({
+            kind: "write",
+            edits: [[ty, tx,0,0,getDate(),char.repeat(128),1,0]]
+        }));
     };
 
     const yValues = IntRange(minY, maxY).map(e => e*-1);
     const xValues = IntRange(minX, maxX);
-    
+
+    let clearingList = [];
+
 
     for(let i = minY; i <= maxY; i++) {
         for(let x = minX; x <= maxX; x++) {
-            wipeTile(x, i);
+            clearingList.push([x, i]);
         };
     };
+    for(var i = 0; i < clearingList.length; i++) {
+        let tile = clearingList[i];
+        setTimeout(()=>{wipeTile(...tile)}, i*7);
+    };
+    clearingList = [];
+};
+function clearTileRangeHelp() {
+    console.log("===IN-CONSOLE MANUAL===\nDo not use outside of spawn.\nLarge selected areas will not be fully cleared, use multiple times.")
 };
 var sel = new RegionSelection();
 sel.charColor = "#ff0000";
@@ -71,5 +90,4 @@ client_commands.clear = ([subcommand]) => {
             clearerChatResponse("Invalid subcommand");
         };
     };
-    
 };
