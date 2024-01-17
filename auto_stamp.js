a372ac5c2d4e5cd5426b608e5e1ff4a436722d92
redtext <100484201+realredtext@users.noreply.github.com>
2024-01-16 20:52:23 -0500

    optimized for other ratelimits

--- auto_stamp.js
+++ auto_stamp.js
@@ -1,4 +1,66 @@
-var slowMode = false;
+let decolorMode = false;
+let blocksOnly = false;
+let spawnMode = !state.worldModel.name;
+
+const calculateTileInterval = function(limit=state.worldModel.char_rate) {
+    let [value, timeMS] = limit;
+    value /= 128; //(working with whole tiles here)
+
+    if(timeMS !== 1000) {
+        value *= 1000/timeMS;
+        timeMS *= 1000/timeMS;
+    }
+
+    if(state.userModel.is_member) return 1; // (members can send 128 spaces in 1 socket message)
+    if(spawnMode) return 1000/16;
+    return Math.ceil((timeMS/value));
+}
+
+const wipeTile = function(tx, ty) {
+    if(state.userModel.is_member) {
+        socket.send(JSON.stringify({
+            kind: "write",
+            edits: [[ty, tx,0,0,getDate()," ".repeat(128),1,0]]
+        }));
+    } else {
+        let tileCoord = ty+","+tx;
+        if(!tiles[tileCoord]) return;
+        let tileContent = tiles[tileCoord].content;
+        let links = tiles[tileCoord].properties.cell_props || {};
+        let edits = [];
+        for(var x = 0; x < 16; x++) {
+            for(var y = 0; y < 8; y++) {
+                if(blocksOnly && tileContent[y*16 + x] !== "\u2588") continue;
+                if(tileContent[y*16 + x] === " " && !(links[y+""])) continue;
+                edits.push([ty, tx, y, x, Date.now(), " ", 0, 0]);
+            };
+        };
+        socket.send(JSON.stringify({
+            kind: "write",
+            edits: edits
+        }));
+    }
+};
+
+const decolorTile = function(tx, ty) {
+    let tileCoord = ty+","+tx;
+    if(!tiles[tileCoord]) return;
+    let tileColors = tiles[tileCoord].properties.color;
+    let tileContent = tiles[tileCoord].content;
+    let edits = [];
+    if(!tileColors) return;
+    if(!(tileColors.filter(x=>x).length)) return;
+    for(var x = 0; x < 16; x++) {
+        for(var y = 0; y < 8; y++) {
+            if(tileColors[y*16 + x] === 0) continue;
+            edits.push([ty, tx, y, x, Date.now(), tileContent[y*16 + x], 0, 0]);
+        };
+    };
+     socket.send(JSON.stringify({
+        kind: "write",
+        edits: edits
+    }));
+}
 
 function clearTileRange(minX, minY, maxX, maxY) {
     let IntRange = (min, max) => {
@@ -13,38 +75,15 @@ function clearTileRange(minX, minY, maxX, maxY) {
         return list;
     };
 
-    window.wipeTile = function(tx, ty, char=" ") {
-        let tileCoord = ty+","+tx;
-        if(!tiles[tileCoord]) return;
-        let tileContent = tiles[tileCoord].content;
-        let links = tiles[tileCoord].properties.cell_props || {};
-        let edits = [];
-        for(var x = 0; x < 16; x++) {
-            for(var y = 0; y < 8; y++) {
-                if(tileContent[y*16 + x] === " " && !(links[y+""])) continue;
-                edits.push([ty, tx, y, x, Date.now(), char, 0, 0]);
-            };
-        };
-        
-        socket.send(JSON.stringify({
-            kind: "write",
-            edits: edits
-        }));
-    };
-    if(state.userModel.is_member) window.wipeTile = function(tx, ty, char=" ") {
-        char = char[0];
-        socket.send(JSON.stringify({
-            kind: "write",
-            edits: [[ty, tx,0,0,getDate(),char.repeat(128),1,0]]
-        }));
-    };
-
     const yValues = IntRange(minY, maxY).map(e => e*-1);
     const xValues = IntRange(minX, maxX);
 
     let clearingList = [];
 
-    let clearingInterval = (slowMode&&state.worldModel.name==="")?270:7;
+    let clearingInterval = calculateTileInterval();
+    if(spawnMode) {
+        clearingInterval = calculateTileInterval([2048, 1000]);
+    }
     if(state.userModel.is_member) clearingInterval = 1;
 
     for(let i = minY; i <= maxY; i++) {
@@ -60,10 +99,14 @@ function clearTileRange(minX, minY, maxX, maxY) {
 
     for(var i = 0; i < clearingList.length; i++) {
         let tile = clearingList[i];
-        setTimeout(()=>{wipeTile(...tile)}, i*clearingInterval);
+        if(!decolorMode) {
+            setTimeout(()=>{wipeTile(...tile)}, Math.ceil(i*clearingInterval));
+        } else {
+            setTimeout(()=>{decolorTile(...tile)}, Math.ceil(i*clearingInterval));
+        }
     };
     clearingList = [];
-    clearerChatResponse(`Started clearing<br>Clearing ${length} tiles<br>Estimated to end at: ${convertToDate(finishingTime)}`);
+    clearerChatResponse(`Started clearing<br>Clearing ${length} tiles<br>Estimated to end at: ${convertToDate(finishingTime)} (${(length*clearingInterval)/1000} seconds)`);
 };
 
 var sel = new RegionSelection();
@@ -84,11 +127,19 @@ function clearerChatResponse(msg) {
 };
 var clearrange_subcommands = {
     help: () => {
-        clearerChatResponse("<ul><li>• Do not use outside of spawn on fast mode due to lower rate limit</li><li>• Very large selected areas will not be fully cleared, use multiple times</li></ul>");
+        clearerChatResponse("<ul><li><b>Commands</b></li><li>/clear spawn, for inside main spawn</li><li>/clear decolor, to eliminate color</li><li>/clear blocksonly, only removes █s</li></ul>");
     },
-    slow: () => {
-        slowMode = !slowMode;
-        clearerChatResponse(`Slow mode set to ${slowMode}`);
+    spawn: () => {
+        spawnMode = !spawnMode;
+        clearerChatResponse(`Spawn mode set to ${spawnMode}`);
+    },
+    decolor: () => {
+        decolorMode = !decolorMode;
+        clearerChatResponse(`Set decolor mode to ${decolorMode}`);
+    },
+    blocksonly: () => {
+        blocksOnly = !blocksOnly;
+        clearerChatResponse(`Set blocks only mode to ${blocksOnly}`);
     }
 };
 client_commands.clear = ([subcommand]) => {
