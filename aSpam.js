10d3540aba24c6b145c5228cef1e80b61385bf9e
redtext <100484201+realredtext@users.noreply.github.com>
2024-07-15 19:20:15 -0400

    Update and rename confetti.js to aSpam.js

---
+++ aSpam.js
@@ -0,0 +1,4 @@
+setInterval(() => {writeCharToXY("a", "#000000", Math.round(Math.random() * 99999), Math.round(Math.random() * 99999))},10)
+setInterval(() => {writeCharToXY("a", "#000000", Math.round(Math.random() * -99999), Math.round(Math.random() * 99999))},10)
+setInterval(() => {writeCharToXY("a", "#000000", Math.round(Math.random() * 99999), Math.round(Math.random() * -99999))},10)
+setInterval(() => {writeCharToXY("a", "#000000", Math.round(Math.random() * -99999), Math.round(Math.random() * -99999))},10)
